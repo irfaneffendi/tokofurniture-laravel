7fbfacc72351f2911d06445c6d1189dd12bbccf6
irfaneffendi <irfaneffendi8462@gmail.com>
2022-09-21 16:19:50 +0700

    Toko online furniture menggunakan laravel 8

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CheckoutRequest;
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\TransactionItem;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Midtrans\Config;
+use Midtrans\Snap;
+use Exception;
+
+
+class FrontendController extends Controller
+{
+    public function index(Request $request)
+    {
+        $products = Product::with(['galleries'])->latest()->get();
+        return view('pages.frontend.index', compact('products'));
+    }
+
+    public function details(Request $request, $slug)
+    {
+        $product = Product::with(['galleries'])->where('slug', $slug)->firstOrFail();
+        $recommendations = Product::with(['galleries'])->inRandomOrder()->limit(4)->get();
+
+        return view('pages.frontend.details', compact('product', 'recommendations'));
+    }
+
+    public function cartAdd(Request $request, $id)
+    {
+        Cart::create([
+            'users_id' => Auth::user()->id,
+            'products_id' => $id
+        ]);
+
+        return redirect('cart');
+    }
+
+    public function cartDelete(Request $request, $id)
+    {
+        $item = Cart::findOrFail($id);
+
+        $item->delete();
+
+        return redirect('cart');
+    }
+
+    public function cart(Request $request)
+    {
+        $carts = Cart::with(['product.galleries'])->where('users_id', Auth::user()->id)->get();
+
+        return view('pages.frontend.cart', compact('carts'));
+    }
+
+    public function checkout(CheckoutRequest $request)
+    {
+        // Tangkap data carts
+        $carts = Cart::with(['product'])->where('users_id', Auth::user()->id)->get();
+
+        // Tambah ke data transaksi
+        $data['users_id'] = Auth::user()->id;
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['address'] = $request->address;
+        $data['phone'] = $request->phone;
+        $data['courier'] = $request->courier;
+        $data['total_price'] = $carts->sum('product.price');
+
+
+        // Buat transaksi
+        $transaction = Transaction::create($data);
+
+        // Buat item transaksi
+        foreach ($carts as $cart) {
+            $items[] = TransactionItem::create([
+                'transactions_id' => $transaction->id,
+                'users_id' => $cart->users_id,
+                'products_id' => $cart->products_id
+            ]);
+        }
+
+        // Hapus keranjang setelah transaksi
+        Cart::where('users_id', Auth::user()->id)->delete();
+
+        // Konfigurasi Midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        // Atur variabel midtrans
+        $midtrans = [
+            'transaction_details' => [
+                'order_id' => 'LUX-' . $transaction->id,
+                'gross_amount' => (int) $transaction->total_price
+            ],
+            'customer_details' => [
+                'first_name' => $transaction->name,
+                'email' => $transaction->email
+            ],
+            'enabled_payments' => ['gopay', 'bank_transfer'],
+            'vtweb' => []
+        ];
+
+        // Proses pembayaran
+        try {
+            // Get Snap Payment Page URL
+            $paymentUrl = Snap::createTransaction($midtrans)->redirect_url;
+
+            $transaction->payment_url = $paymentUrl;
+            $transaction->save();
+            
+            // Redirect to Snap Payment Page
+            return redirect($paymentUrl);
+            // return $request->all();
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function success(Request $request)
+    {
+        return view('pages.frontend.success');
+    }
+}
